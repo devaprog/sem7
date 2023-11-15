92fa7eea8cd0487e1377e531805c5d90a41ef31f
devaprog <127442537+devaprog@users.noreply.github.com>
2023-11-15 15:39:06 +0530

    better key generation mechanism in des.py

--- des.py
+++ des.py
@@ -1,44 +1,24 @@
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives import padding
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import padding as asy_padding
-from cryptography.hazmat.primitives import hashes
+from Crypto.Random import get_random_bytes
 
 class DES:
     def __init__(self):
         try:
-            self.inputmessage = input("Enter message to encrypt: ")
-            self.key = self.generate_symmetric_key()
-            self.encrypted_data = self.encrypt(self.key, self.inputmessage)
-            print("Encrypted message:", self.encrypted_data.decode())
-            decrypted_message = self.decrypt(self.key, self.encrypted_data)
+            message = input("Enter message to encrypt: ")
+            key = get_random_bytes(8)  # 8 bytes for DES
+            encrypted_data = self.crypt(message, key, 'encrypt')
+            print("Encrypted message:", encrypted_data.decode())
+            decrypted_message = self.crypt(encrypted_data, key, 'decrypt')
             print("Decrypted message:", decrypted_message.decode())
+            print("DES Symmetric Key:", key.hex())
         except Exception as e:
             print(e)
 
-    def generate_symmetric_key(self):
-        key = b"mysecretpassword"  # Replace with a strong key generation mechanism
-        return key
-
-    def encrypt(self, key, plaintext):
-        cipher = Cipher(algorithms.TripleDES(key), modes.ECB())
-        encryptor = cipher.encryptor()
-        padder = padding.PKCS7(64).padder()
-        padded_data = padder.update(plaintext.encode()) + padder.finalize()
-        encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
-        return encrypted_data
-
-    def decrypt(self, key, ciphertext):
-        cipher = Cipher(algorithms.TripleDES(key), modes.ECB())
-        decryptor = cipher.decryptor()
-        decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
-        unpadder = padding.PKCS7(64).unpadder()
-        unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
-        return unpadded_data
-
+    def crypt(self, data, key, mode):
+        cipher = DES3.new(key, DES3.MODE_ECB)
+        if mode == 'encrypt':
+            return b64encode(cipher.encrypt(pad(data.encode(), DES3.block_size)))
+        elif mode == 'decrypt':
+            return unpad(cipher.decrypt(b64decode(data)), DES3.block_size).decode()
 
 if __name__ == "__main__":
     des = DES()
